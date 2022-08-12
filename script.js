b1262c694903ab53815d8b8d56913bf4ce64fdf7
IshakAtes <ishakfeuer@gmail.com>
2022-08-12 12:41:31 +0200

    fix the PokemonCard and text-decoration// next-Step style the PokemonLogo with Z-index

--- script.js
+++ script.js
@@ -24,23 +24,29 @@ async function renderPokemon(responseAsJson){
         // console.log(responsePokemonSpecies);
         let pokeId = idFormater(responsePokemonSpecies['id']);
         let renderContainer = document.getElementById('renderPokemonContainer');
-        renderContainer.innerHTML += `
-            <div class="pokemonCave allCenter" style="background-color: ${responsePokemonSpecies['color']['name']};">
-                <div class="pokemonInfoContainer">
-                    <h3>${element['name']}</h3>
-                    <span class="spanStyle mb-2">${responsePokemonData['types'][0]['type']['name']}</span>
-                    <span class="spanStyle mb-2">${responsePokemonSpecies['habitat']['name']}</span>
-                </div>
-                <div>
-                    <div style="text-align: right; margin-right: 10px;">${pokeId}</div>
-                    <img src="${responsePokemonData['sprites']['other']['home']['front_default']}" class="img-fluid" alt="">     
-                </div>
-            </div>
-        `;
+
+        renderContainer.innerHTML += generatePokemonCardTemplateHTML(responsePokemonSpecies, element, responsePokemonData, pokeId);
     }
     if (responseAsJson['next'] == `https://pokeapi.co/api/v2/pokemon?offset=${pokeCounter}&limit=20`) {
         loadNextPokemons();
     }
+}
+
+function generatePokemonCardTemplateHTML(responsePokemonSpecies, element, responsePokemonData, pokeId){
+    return `
+        <div class="pokemonCave allCenter" style="background-color: ${responsePokemonSpecies['color']['name']};">
+            <div class="pokemonInfoContainer">
+                <h3 class="textShadow">${element['name']}</h3>
+                <span class="spanStyle mb-2 textShadow">${responsePokemonData['types'][0]['type']['name']}</span>
+                <span class="spanStyle mb-2 textShadow">${responsePokemonSpecies['habitat']['name']}</span>
+            </div>
+            <div>
+                <div class="textShadow" style="text-align: right; margin-right: 10px;">${pokeId}</div>
+                <img class="pokemonsImg" src="${responsePokemonData['sprites']['other']['home']['front_default']}" class="img-fluid" alt="">
+                <img class="pokeballImg" src="img/catching_pokemon_white_24dp.svg" alt="">     
+            </div>
+        </div>
+    `;
 }
 
 function idFormater(responseId){
@@ -53,7 +59,6 @@ async function loadNextPokemons(){
     let nextUrl = `https://pokeapi.co/api/v2/pokemon?offset=${pokeCounter}&limit=20`;
     let response = await fetch(nextUrl);
     let responseNextPokemonJson = await response.json();
-    console.log(responseNextPokemonJson);
     pokeCounter = pokeCounter + 20;
     renderPokemon(responseNextPokemonJson);
 }
