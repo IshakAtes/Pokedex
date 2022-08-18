722a61a1bbc433bef4560c94f7fbab6f0209a808
IshakAtes <ishakfeuer@gmail.com>
2022-08-18 15:02:37 +0200

    added onclick-function for renderPokemonCards

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ async function responseApi(){
     let response = await fetch(url);
     let responseAsJson = await response.json();
     console.log(responseAsJson)
-    renderPokemon(responseAsJson)
+    getPokemonDatas(responseAsJson)
 }
 
-async function renderPokemon(responseAsJson){
+
+async function getPokemonDatas(responseAsJson){
     for (let i = 0; i < responseAsJson['results'].length; i++) {
         const element = responseAsJson['results'][i];
         let pokemonData = await fetch(element['url']);
@@ -23,18 +24,19 @@ async function renderPokemon(responseAsJson){
         let responsePokemonSpecies = await pokemonSpecies.json();
         // console.log(responsePokemonSpecies);
         let pokeId = idFormater(responsePokemonSpecies['id']);
-        let renderContainer = document.getElementById('renderPokemonContainer');
 
-        renderContainer.innerHTML += generatePokemonCardTemplateHTML(responsePokemonSpecies, element, responsePokemonData, pokeId);
+        renderPokemon(responsePokemonSpecies, element, responsePokemonData, pokeId);
     }
-    if (responseAsJson['next'] == `https://pokeapi.co/api/v2/pokemon?offset=${pokeCounter}&limit=20`) {
-        loadNextPokemons();
-    }
+    // if (responseAsJson['next'] == `https://pokeapi.co/api/v2/pokemon?offset=${pokeCounter}&limit=20`) {
+    //     loadNextPokemons();
+    // }
 }
 
-function generatePokemonCardTemplateHTML(responsePokemonSpecies, element, responsePokemonData, pokeId){
-    return `
-        <div class="pokemonCave allCenter" style="background-color: ${responsePokemonSpecies['color']['name']};">
+
+function renderPokemon(responsePokemonSpecies, element, responsePokemonData, pokeId){
+    let renderContainer = document.getElementById('renderPokemonContainer');
+    renderContainer.innerHTML += `
+        <div onclick="renderPokemonCard('${element['name']}')" class="pokemonCave allCenter" style="background-color: ${responsePokemonSpecies['color']['name']};">
             <div class="pokemonInfoContainer">
                 <h3 class="textShadow">${element['name']}</h3>
                 <span class="spanStyle mb-2 textShadow">${responsePokemonData['types'][0]['type']['name']}</span>
@@ -49,16 +51,38 @@ function generatePokemonCardTemplateHTML(responsePokemonSpecies, element, respon
     `;
 }
 
+
+async function renderPokemonCard(pokeName){
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    let responsePokemon = await fetch(url);
+    let responsePokemonInfoAsJson = await responsePokemon.json();
+    console.log(responsePokemonInfoAsJson);
+    let pokemonSpecies = await fetch(responsePokemonInfoAsJson['species']['url'])
+    let responsePokemonSpecies = await pokemonSpecies.json();
+
+    document.getElementById('overlay').style.display = 'unset';
+    document.getElementById('renderPokemonCards').style.display = 'flex';
+    let renderCard = document.getElementById('pokemonCardContainer')
+    renderCard.innerHTML = '';
+    renderCard.innerHTML = `
+    <div id="pokeCard" class="allCenter cardStyle" style="background-color: ${responsePokemonSpecies['color']['name']};">
+        <img src="${responsePokemonInfoAsJson['sprites']['other']['home']['front_default']}" class="img-fluid" alt="">
+    </div>
+    `;
+}
+
+
 function idFormater(responseId){
     if(responseId < 10){return ('#00' + responseId)};
     if(responseId < 100){return ('#0' + responseId)};
     if(responseId >= 100){return ('#' + responseId)};
 }
 
+
 // async function loadNextPokemons(){
 //     let nextUrl = `https://pokeapi.co/api/v2/pokemon?offset=${pokeCounter}&limit=20`;
 //     let response = await fetch(nextUrl);
 //     let responseNextPokemonJson = await response.json();
 //     pokeCounter = pokeCounter + 20;
-//     renderPokemon(responseNextPokemonJson);
+//     getPokemonDatas(responseNextPokemonJson);
 // }
